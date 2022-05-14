bb543235e1f6ca0675f8ae077883ee7d5aa81086
Divya2398 <102854040+Divya2398@users.noreply.github.com>
2022-05-14 20:20:01 +0530

    Delete dbtable.py

--- dbtable.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.Connect(host="localhost", user="root", password="project", database="employe")
-
-mycursor = mydb.cursor()
-# mycursor.execute("create table records(name varchar(200), salary int(20))")
-mycursor.execute("show tables")
-
-for tb in mycursor:
-    print(tb)
