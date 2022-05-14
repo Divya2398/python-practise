742079acc1012f32fb3770abc2bd96c2c182462a
Divya2398 <102854040+Divya2398@users.noreply.github.com>
2022-05-14 20:20:12 +0530

    Delete dbupdate.py

--- dbupdate.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.Connect(host="localhost", user="root", password="project", database="employe")
-
-mycursor = mydb.cursor()
-
-update = "Update records SET salary=25000 where name='Divya'"
-mycursor.execute(update)
-mydb.commit()
