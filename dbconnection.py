a4f513040550078113f2257cd15c7f4d05b80b38
Divya2398 <102854040+Divya2398@users.noreply.github.com>
2022-05-14 20:19:01 +0530

    Delete dbconnection.py

--- dbconnection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-mydb=mysql.connector.Connect(host="localhost", user="root", password="project")
-print(mydb)
-if(mydb):
-    print("connection success")
-else:
-    print("connection failure")
